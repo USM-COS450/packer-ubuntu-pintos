75bb8a4a732301e66db861a2d1d0988adb1c1900
Stephen Houser <stephenhouser@gmail.com>
2018-08-02 21:42:00 -0400

    try pre-packaged bochs

--- script/pintos-dev.sh
+++ script/pintos-dev.sh
@@ -4,9 +4,25 @@ PINTOS_USER=${SSH_USERNAME:-pintos}
 
 PINTOS_HOME=/home/${PINTOS_USER}
 
-echo "==> Install Development Dependencies and QEMU"
+echo "==> Install QEMU emulator"
 # install build-essential git qemu-system-x86 ncurses-dev
-apt-get -y install qemu-system-x86 ncurses-dev
+apt-get -y install qemu-system-x86 
+#ncurses-dev
+
+# echo "==> Build and install Bochs emulator"
+# cd $PINTOS_HOME
+# curl -O -J -L  https://sourceforge.net/projects/bochs/files/bochs/2.2.6/bochs-2.2.6.tar.gz/download
+
+# SRCDIR=$PINTOS_HOME \
+# 	PINTOSDIR=$PINTOS_HOME/pintos \
+# 	DSTDIR=/usr/local \
+# 	$PINTOS_HOME/pintos/src/misc/bochs-2.2.6-build.sh
+
+if [ -f /tmp/bochs-2.2.6-bin-pintos.tar.gz ] ; then
+	echo "==> Install Bochs emulator"
+	cd /usr/local
+	tar xzf /tmp/bochs-2.2.6-bin-pintos.tar.gz --strip-components=2
+fi
 
 echo "==> Clone Pintos"
 cd $PINTOS_HOME
@@ -17,19 +33,11 @@ cd pintos/src/utils && \
 	make && \
 	find . -executable -type f -exec cp {} /usr/local/bin \;
 
-echo "==> Build and install Bochs emulator"
-cd $PINTOS_HOME
-curl -O -J -L  https://sourceforge.net/projects/bochs/files/bochs/2.2.6/bochs-2.2.6.tar.gz/download
 
-SRCDIR=$PINTOS_HOME \
-	PINTOSDIR=$PINTOS_HOME/pintos \
-	DSTDIR=/usr/local \
-	$PINTOS_HOME/pintos/src/misc/bochs-2.2.6-build.sh
-
-echo "==> Clean up Pintos, Bochs, and Build Artifacts"
+echo "==> Clean up Pintos and build artifacts"
 #cd $PINTOS_HOME
 #rm -rf pintos bochs-2.2.6.tar.gz
 
-apt-get -y purge ncurses-dev
-apt-get -y autoclean
-apt-get -y clean
+# apt-get -y purge ncurses-dev
+# apt-get -y autoclean
+# apt-get -y clean
