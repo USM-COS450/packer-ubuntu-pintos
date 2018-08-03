09f69db86b982266c4517d50bb2db4d5bb702bf9
Stephen Houser <stephenhouser@gmail.com>
2018-08-03 13:23:20 -0400

    update Pintos utils install and bochs install

--- script/pintos-dev.sh
+++ script/pintos-dev.sh
@@ -1,43 +1,35 @@
 #!/bin/bash -eu
 
 PINTOS_USER=${SSH_USERNAME:-pintos}
-
 PINTOS_HOME=/home/${PINTOS_USER}
 
+PINTOS_GIT=${PINTOS_GIT_URL:-https://github.com/USM-COS450/pintos.git}
+BOCHS_BIN_TARBALL=${BOCHS_PRECOMPILED_BIN:-$PINTOS_HOME/bochs-2.2.6-bin-pintos.tar.gz}
+BOCHS_SRC_URL=https://sourceforge.net/projects/bochs/files/bochs/2.2.6/bochs-2.2.6.tar.gz/download
+
 echo "==> Install QEMU emulator"
-# install build-essential git qemu-system-x86 ncurses-dev
 apt-get -y install qemu-system-x86 
-#ncurses-dev
 
-# echo "==> Build and install Bochs emulator"
-# cd $PINTOS_HOME
-# curl -O -J -L  https://sourceforge.net/projects/bochs/files/bochs/2.2.6/bochs-2.2.6.tar.gz/download
-
-# SRCDIR=$PINTOS_HOME \
-# 	PINTOSDIR=$PINTOS_HOME/pintos \
-# 	DSTDIR=/usr/local \
-# 	$PINTOS_HOME/pintos/src/misc/bochs-2.2.6-build.sh
-
-if [ -f /tmp/bochs-2.2.6-bin-pintos.tar.gz ] ; then
-	echo "==> Install Bochs emulator"
+# For bochs
+apt-get -y install libx11-6 libxpm4
+if [ -f $BOCHS_BIN_TARBALL ] ; then
+	echo "==> Install prebuilt Bochs emulator"
 	cd /usr/local
-	tar xzf /tmp/bochs-2.2.6-bin-pintos.tar.gz --strip-components=2
+	tar xzf $BOCHS_BIN_TARBALL --strip-components=2
+	rm -f $BOCHS_BIN_TARBALL
+else
+	echo "==> Build and install Bochs emulator"
+	apt-get -y install libx11-6-dev libxpm4-dev ncurses-dev
+	cd $PINTOS_HOME
+	curl -O -J -L $BOCHS_SRC_URL
+	SRCDIR=$PINTOS_HOME \
+		PINTOSDIR=$PINTOS_HOME/pintos \
+		DSTDIR=/usr/local \
+		$PINTOS_HOME/pintos/src/misc/bochs-2.2.6-build.sh
 fi
 
-echo "==> Clone Pintos"
-cd $PINTOS_HOME
-git clone https://github.com/USM-COS450/pintos.git
-
 echo "==> Build and install Pintos utilities"
-cd pintos/src/utils && \
-	make && \
-	find . -executable -type f -exec cp {} /usr/local/bin \;
-
-
-echo "==> Clean up Pintos and build artifacts"
-#cd $PINTOS_HOME
-#rm -rf pintos bochs-2.2.6.tar.gz
-
-# apt-get -y purge ncurses-dev
-# apt-get -y autoclean
-# apt-get -y clean
+cd $PINTOS_HOME
+git clone $PINTOS_GIT
+(cd pintos/src/utils && make install)
+rm -rf pintos 
